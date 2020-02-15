8e696eb9a72ced2473e8a8fe2cbea8ea30c1c275
StevenVallarsa <orionsjv@gmail.com>
2020-02-14 21:02:22 -0500

    Added classes

--- Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Board.js
+++ Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Board.js
@@ -1,1 +1,36 @@
-class Board {}
+class Board {
+  constructor() {
+    this.rows = 6;
+    this.columns = 7;
+    this.spaces = this.createSpaces();
+  }
+
+  /**
+   * Generates 2D array of spaces.
+   * @return {Array}  An array of space objects
+   */
+  createSpaces() {
+    const spaces = [];
+
+    for (let x = 0; x < this.columns; x++) {
+      const column = [];
+
+      for (let y = 0; y < this.rows; y++) {
+        column.push(new Space(x, y));
+      }
+      spaces.push(column);
+    }
+    return spaces;
+  }
+
+  /**
+   * Draws associated SVG spaces for all game spaces.
+   */
+  drawHTMLBoard() {
+    for (let column of this.spaces) {
+      for (let space of column) {
+        space.drawSVGSpace();
+      }
+    }
+  }
+}
--- Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Player.js
+++ Stage1 - Introducing Four in a Row/Stage1Step1 - Introducing the Project/js/Player.js
@@ -7,11 +7,34 @@ class Player {
     this.token = this.createTokens(21);
   }
 
+  /**
+   * Creates token objects for player
+   * @param   {integer}   num - Number of token objects to be created
+   * @return  {array}     tokens - an array of new token objects
+   */
+
   createTokens(number) {
     const tokens = [];
     for (let i = 0; i < number; i++) {
-      tokens.push(new Token(i, this.name));
+      tokens.push(new Token(i, this));
     }
+    console.log(tokens);
     return tokens;
+  }
+
+  /**
+   * Gets all togens that haven't been dropped.
+   * @return {array}  Array of unused tokens.
+   */
+  get unusedTokens() {
+    return this.token.filter(token => !token.dropped);
+  }
+
+  /**
+   * Gets the active token by returning the first toekn in the array of unused tokens.
+   * @return {Object}   First token object in the array of unused tokens.
+   */
+  get activeToken() {
+    return this.unusedTokens[0];
   }
 }
